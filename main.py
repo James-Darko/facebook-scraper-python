8069316172173ef454b8566a57da4804549d6b7c
James-Darko <will.j.moorhead@gmail.com>
2022-08-31 09:53:07 -0500

    *

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ version = 6
 def to_json(dict_var):
     return json.dumps(dict_var, default=str)
 
-if len(sys.argv) < 5 and sys.argv[1] != "version":
-    print("ERROR: wrong number of arguments")
-    exit(1)
-
 user_agents = [
     'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/103.0.5060.63 Mobile/15E148 Safari/604.1',
     'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/104.0.5112.99 Mobile/15E148 Safari/604.1',
@@ -21,13 +17,23 @@ user_agents = [
 ]
 
 try:
+    command = sys.argv[1]
+    if command == "version":
+        print(f"version {version}")
+        exit(0)
+
+    if len(sys.argv) < 5:
+        print("ERROR: wrong number of arguments")
+        exit(1)
+
+    if len(sys.argv) == 6 and sys.argv[5] == "log":
+        facebook_scraper.enable_logging()
+
     proxy = sys.argv[4]
     facebook_scraper.set_proxy(proxy)
     print(f"facebook-scraper-python v{version} - {sys.argv[2]} - {proxy}")
-    if len(sys.argv) == 6 and sys.argv[5] == "log":
-        facebook_scraper.enable_logging()
-    command = sys.argv[1]
     if command == "feed":
+
         username = sys.argv[2]
         limit = int(sys.argv[3])
         cookies = {
@@ -54,8 +60,6 @@ try:
         with open(file, "w") as f:
             post = next(posts)
             f.write(to_json(post))
-    elif command == "version":
-        print(f"version {version}")
     else:
         print("unknown command: " + command)
         exit(1)
